1063d6d473dd2ade8e72434a2f5d3b0461fe79a3
Francef <56524002+Francef@users.noreply.github.com>
2022-07-27 16:31:22 -0700

    Big changes in structure
    Removed the table output of function loadDrinks() in favour of presenting information on page with images. Played with the .hide() and .show() functionality of buttons. Changed the JSON loading to happen upon page load. There are now proper images and links to recipes on our website, they just need filling out.

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
 let listData; 
 let table;
+load_data_with_fetch();		// initial fetch upon load of page
+$(`#drinkTable`).hide(); // keep this hidden until user presses "Load all drinks"
+$(`#refineDrinkList`).hide(); // keep this hidden until user presses "Load all drinks"
 
 /* 	Function load_data_with_fetch
 	Purpose: provides fetch of JSON data and assigns it to the variable "listData". In addition, it populates
 	the page with a table displaying all drinks available */
-	
 function load_data_with_fetch() {
         fetch("https://francef.github.io/final.json"). // link to JSON API
             then(response => response.json()).
             then((json) => {
                 let i;
 				let j;
-                table = `<tr><th>Drink Name</th><th>Alcohol required</th><th></th><th></th><th></th><th></th><th>Recipe</th></tr>`;      // creates the table's headings
+                table = `<tr><th>Drink Name</th><th>Alcohol required</th><th>Recipe</th></tr>`;      // creates the table's headings
                 listData = json;
 				for (i = 0; i < listData.length; i++) { 
 				 // creates each row of the table
                     table += `<tr><td>` + listData[i].drink_name + `</td><td>`;
 					for (j = 0; j < listData[i].alcohol.length; j++) {
-						table += listData[i].alcohol[j] + `</td><td>`;
+						if (j < listData[i].alcohol.length -1) {
+							table += listData[i].alcohol[j] + `, `;
+						} else {
+							table += listData[i].alcohol[j];
+						}
 					}
-					table += `<a href=` + listData[i].recipe_link + `</a></td></tr>`;
+					table += `</td><td><a href=` + listData[i].recipe_link + `</a></td></tr>`;
                 }
 
-				$(`#drinkTable`).append(table);
+				$(`#drinkTable`).html(table);
             });
     }
 	
 function loadDrinks(liquor) {
-	table = `<tr><th>Drink Name</th><th>Alcohol required</th><th></th><th></th><th></th><th></th><th>Recipe</th></tr>`;
+	let youCanMake = `<h4>You can make the following drinks: </h4>`;
 	for (i = 0; i < listData.length; i++) { 
-
-					for (j = 0; j < listData[i].alcohol.length; j++) {
-						if (listData[i].alcohol[j] == liquor) {
-						 // creates each row of the table
-						table += `<tr><td>` + listData[i].drink_name + `</td><td>`;
-						table += listData[i].alcohol[j] + `</td><td>`;
+		for (j = 0; j < listData[i].alcohol.length; j++) {
+			if (listData[i].alcohol[j] == liquor) {
+				youCanMake += `<h4>` + listData[i].drink_name + `</h4>`;
+					if (listData[i].drinkImage != "") {
+						youCanMake += `<img src="` + listData[i].drinkImage + `">`;
 					}
-					table += `<a href=` + listData[i].recipe_link + `</a></td></tr>`;
+					if (listData[i].recipe_link != "") {
+						youCanMake += `<p>Recipe: <a href=` + listData[i].recipe_link + `</a></p>`;
+					}
                 }
-				document.querySelector(`#drinkTable`).innerHTML = table;
+	$(`#drinkTable`).hide(); // hide drinks table when a selection is made
+	$(`#showRefinedList`).html(youCanMake);
+	}
+	}
 }
-}
+
 
 let selectAlcohol = document.querySelector(`#selectLiquor`);
 selectAlcohol.addEventListener('change', (event) => {
@@ -48,3 +58,9 @@ selectAlcohol.addEventListener('change', (event) => {
 		loadDrinks(drink);
 		console.log(drink);
 });
+
+function showAll() {
+	$(`#drinkTable`).show(); // show drink table only when user selects "Load All Drinks"
+	$(`#loadAll`).hide();
+	$(`#refineDrinkList`).show();
+}
