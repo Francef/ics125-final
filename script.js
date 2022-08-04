ddc1a89d0877b28073bef5c66cf544899f63e395
Francef <56524002+Francef@users.noreply.github.com>
2022-08-03 22:36:13 -0700

    Added login function and randomDrink() function

--- script.js
+++ script.js
@@ -6,6 +6,82 @@ load_data_with_fetch();									// initial fetch upon load of page
 $(`#drinkTable`).hide(); 								// keep this hidden until user presses "Load all drinks"
 $(`#refineDrinkList`).hide(); 							// keep this hidden until user presses "Load all drinks"
 
+// login event handler
+ $(`#login-button`).on(`click`, function (event) {
+        // prevent the form from submitting
+        event.preventDefault();
+        // keep track of the form validity
+        let validity = true;
+        // validate the email field
+        if ($(`#login-email`).val() == ``) {
+            //use the bootstrap built in styles to indicate a problem with the fiekd
+            $(`#login-email`).addClass(`is-invalid`);
+
+            // add a hover tooltip to the #login-email fields
+            let tooltip = new bootstrap.Tooltip(`#login-email`, {
+                title: `Email cannot be blank`
+            });
+
+            // set the validity to false
+            validity = false;
+        } else {
+            // remove the is-invalid class, if it exists, from the login-email field
+            $(`#login-email`).removeClass(`is-invalid`);
+
+            // add the css class "is-valid" to give positive feedback to the user
+            $(`#login-email`).addClass(`is-valid`);
+
+            // remove any tooltip on this element
+            if ($(`#login-email`).tooltip != undefined) {
+                $(`#login-email`).tooltip(`dispose`);
+            }
+        }
+
+        // validate the password field
+        if ($(`#login-password`).val() == ``) {
+            //use the bootstrap built in styles to indivate a problem with the fiekd
+            $(`#login-password`).addClass(`is-invalid`);
+
+            // add a hover tooltip to the #login-password fields
+            let tooltip = new bootstrap.tooltip(`#login-password`, {
+                title: `Password cannot be blank`
+            });
+
+            // set the validity to false
+            validity = false;
+        } else {
+            // remove the is-invalid class, if it exists, from the login-password field
+            $(`#login-password`).removeClass(`is-invalid`);
+
+            // add the css class "is-invalid" to give positive feedback to the user
+            $(`#login-password`).addClass(`is-valid`);
+
+            // remove any tooltip on this element
+            if ($(`#login-password`).tooltip != undefined) {
+                $(`#login-password`).tooltip(`dispose`);
+            }
+        }
+        if (!validity) {
+            return false;
+        } else {
+            // give class of "spin" to login-spinner if form is valid
+            $(`#login-spinner`).addClass(`spin`);
+        }
+        $(`#login-button`).attr(`disabled`, `disabled`);
+
+        $(`#loginModal`).animate({ opacity: 0 }, 2000, function () {
+            // what we are going to do once the animation completes
+            // hide modal
+            $(`#loginModal`).modal(`hide`);
+
+            // hide login button
+            $(`#part-1`).hide();
+
+            // show part 2 of the exercise
+            $(`#part-2`).show();
+        });
+    }); // end click handler
+
 /* 	Function load_data_with_fetch
 	Purpose: provides fetch of JSON data and assigns it to the variable "listData". In addition, it populates
 	the page with a table displaying all drinks available */
@@ -68,4 +144,21 @@ function showAll() {
 	$(`#drinkTable`).show(); // show drink table only when user selects "Load All Drinks"
 	$(`#loadAll`).hide();
 	$(`#refineDrinkList`).show();
+}
+
+
+/* 	Function randomDrink
+	Purpose: Generate a random index between 0 and the last index of listData, then retrieve the cocktail name, image, and recipe for display on the page */
+function randomDrink() {
+	let totalCocktails = listData.length;
+	let randomIndex = Math.floor(Math.random() * totalCocktails);					// generates a random integer between 0 and 5, inclusive
+	let randomDrinkIs = `<h4>Your random drink is: ` + listData[randomIndex].drink_name + `</h4>`;
+	if (listData[randomIndex].drinkImage != "") {									// if the drinkImage attribute is not empty, display on screen
+		randomDrinkIs += `<img src="` + listData[randomIndex].drinkImage + `">`;
+	}
+	if (listData[randomIndex].recipe_link != "") {									// if the recipe_link is not empty, display the recipe link on screen
+		randomDrinkIs += `<p>Recipe: <a href=` + listData[randomIndex].recipe_link + `</a></p>`;
+	}
+	$(`#drinkTable`).hide(); 														// hide drinks table when random button is chosen
+	$(`#showRefinedList`).html(randomDrinkIs);										// display random drink, image, recipe 
 }
